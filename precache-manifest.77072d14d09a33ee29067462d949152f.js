bd2e11bf74c494af9af675e636fb73193fd59e20
NikeshSRaj <nikeshsraj@gmail.com>
2019-12-30 23:54:22 -0500

    Updates

---
+++ precache-manifest.77072d14d09a33ee29067462d949152f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446579005bd630553d674f42b5e1ea27",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "e2429c2e5a692dd1a215",
+    "url": "/memory-game/static/css/main.c4a96fed.chunk.css"
+  },
+  {
+    "revision": "626304f9b95f6bd20f6f",
+    "url": "/memory-game/static/js/2.50df4e4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory-game/static/js/2.50df4e4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2429c2e5a692dd1a215",
+    "url": "/memory-game/static/js/main.ce6f53a2.chunk.js"
+  },
+  {
+    "revision": "27bb3e093d76e9005e99",
+    "url": "/memory-game/static/js/runtime-main.457aaf77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
